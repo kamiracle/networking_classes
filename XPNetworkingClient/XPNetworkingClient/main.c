c699df8ee3833e33ee034fcc43d0a2f0b6aeea7f
Kyle Miracle <adagev2@gmail.com>
2012-04-19 22:44:38 -0400

    added client and server prototypes

---
+++ XPNetworkingClient/XPNetworkingClient/main.c
@@ -0,0 +1,67 @@
+//
+//  main.c
+//  XPNetworkingClient
+//
+//  Created by Kyle Miracle on 4/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+
+
+#define BUFSIZE 200
+
+int main(int argc, const char * argv[])
+{
+
+    int n, sockfd;
+    short inPort = 666;
+    char buffer[BUFSIZE + 1];
+    struct sockaddr_in serverAddress;
+    
+    if ( argc < 2 ) {
+        printf("%s", "Usage: XPNetworkingClient ipaddress\n");
+        exit(1);
+
+    }
+    
+    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+        perror( "socket" );
+        exit(1);
+    }
+    else {
+        printf("Successful call of socket()\n");
+    }
+        
+    bzero(&serverAddress, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(inPort);
+    
+    inet_pton( AF_INET, argv[1], &serverAddress.sin_addr);
+    
+    if ( connect( sockfd, (struct sockaddr *)&serverAddress,
+                 sizeof(serverAddress)) < 0 ) 
+    {
+        perror("connect");
+        exit(1);
+    }
+    
+    while ( (n = read( sockfd, buffer, 200)) > 0 ) {
+        buffer[n] = 0;
+        printf("%s",buffer);
+    }
+    
+    
+    
+    
+    
+}
+
